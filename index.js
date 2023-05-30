d8e03326b7f3059d9beef4b1af585e8686a2e4f2
DavitGalstian <davgalstiannn@gmail.com>
2023-05-30 19:49:03 +0400

    filterbutton

--- index.js
+++ index.js
@@ -19,10 +19,8 @@ function editToDo(e) {
   saveTodo.textContent = "Save";
   saveTodo.classList.add("toDoListbuttons");
   e.target.parentNode.appendChild(saveTodo);
-  saveTodo.addEventListener("click", saveToDo );
+  saveTodo.addEventListener("click", saveToDo);
 }
-
-
 
 function saveToDo(e) {
   let input = e.target.parentNode.querySelector("input");
@@ -31,6 +29,13 @@ function saveToDo(e) {
   e.target.parentNode.appendChild(newparagraph);
   input.remove();
   e.target.remove();
+}
+
+function filterMarkAsDone(e) {
+  let filteredparagraph = e.target.parentNode.querySelector("p");
+  if (filteredparagraph.style.color !== "green") {
+    filteredparagraph.style.display = "none";
+  }
 }
 
 function submitForm() {
@@ -56,21 +61,27 @@ function submitForm() {
   let editbutton = document.createElement("button");
   editbutton.textContent = "Edit";
   editbutton.classList.add("toDoListbuttons");
+  let filterbutton = document.createElement("button");
+  filterbutton.textContent = "Filter";
+  filterbutton.classList.add("toDoListbuttons");
+
   newdiv.appendChild(newtext);
   newdiv.appendChild(deletebutton);
   newdiv.appendChild(greenbutton);
   newdiv.appendChild(editbutton);
+  newdiv.appendChild(filterbutton);
 
   display.appendChild(newdiv);
   deletebutton.addEventListener("click", removeToDo);
   greenbutton.addEventListener("click", changeTextColor);
   editbutton.addEventListener("click", editToDo);
-
+  filterbutton.addEventListener("click", filterMarkAsDone);
 }
+  
 
 function submitToDo() {
   let submitButton = document.querySelector(".submit");
-  submitButton.addEventListener("click", submitForm); 
+  submitButton.addEventListener("click", submitForm);
 }
 
 submitToDo();
