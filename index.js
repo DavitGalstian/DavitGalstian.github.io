19802f15fe76333e4cb502feccbc4b0db5a0a166
DavitGalstian <davgalstiannn@gmail.com>
2023-06-02 21:04:02 +0400

    toDo

--- index.js
+++ index.js
@@ -1,126 +1,40 @@
-function removeToDo(e) {
-  e.target.parentNode.remove();
+function generateId() {
+  let id = 0;
+  return function() {
+    return id++;
+  };
 }
 
-function changeTextColor(e) {
-  let newText = e.target.parentNode.querySelector("p");
-  let parent = e.target.parentNode;
-  newText.style.color = "green";
-  newText.classList.add("newText");
-  parent.classList.add("doneToDo");
-  iftrue = true;
-}
-function editToDo(e) {
-  let paragraph = e.target.parentNode.querySelector("p");
-  let newinput = document.createElement("input");
-  console.log(paragraph);
-  newinput.value = paragraph.textContent;
-  paragraph.remove();
+let getid = generateId();
 
-  e.target.parentNode.appendChild(newinput);
-  let saveTodo = document.createElement("button");
-  saveTodo.textContent = "Save";
-  saveTodo.classList.add("toDoListbuttons");
-  e.target.parentNode.appendChild(saveTodo);
-  saveTodo.addEventListener("click", saveToDo);
-}
-
-function saveToDo(e) {
-  let input = e.target.parentNode.querySelector("input");
-  let updatedParagraph = document.createElement("p");
-  updatedParagraph.classList.add("saveParagraph");
-  updatedParagraph.textContent = input.value;
-  e.target.parentNode.appendChild(updatedParagraph);
-  if(iftrue==true){
-    updatedParagraph.style.color="green"
-  }
-  input.remove();
-  e.target.remove();
-
-}
-
-
-function submitForm() {
-  let input = document.querySelector(".a");
-  if (!input.value) {
-    return;
-  }
+function render(toDo) {
   let display = document.querySelector(".display");
 
-  let newtext = document.createElement("p");
-  let newdiv = document.createElement("div");
-  newdiv.classList.add("toDoListDiv");
-  newtext.classList.add("toDoListP");
-  let deletebutton = document.createElement("button");
-  deletebutton.textContent = "Delete";
-  deletebutton.classList.add("toDoListbuttons");
-  newtext.textContent = input.value;
-  input.value = "";
-  let greenbutton = document.createElement("button");
-  greenbutton.textContent = "Mark as done";
-  greenbutton.classList.add("toDoListbuttons");
-  let editbutton = document.createElement("button");
-  editbutton.textContent = "Edit";
-  editbutton.classList.add("toDoListbuttons");
+  for (let i = 0; i < toDo.length; i++) {
+    let newDiv = document.createElement("div");
 
+    let newText = document.createElement("p");
+    newDiv.appendChild(newText);
 
-  newdiv.appendChild(newtext);
-  newdiv.appendChild(deletebutton);
-  newdiv.appendChild(greenbutton);
-  newdiv.appendChild(editbutton);
+    let deleteButton = document.createElement("button");
+    deleteButton.textContent = "Delete";
+    newDiv.appendChild(deleteButton);
 
+    let doneButton = document.createElement("button");
+    doneButton.textContent = "Done";
+    newDiv.appendChild(doneButton);
 
-  display.appendChild(newdiv);
-  deletebutton.addEventListener("click", removeToDo);
-  greenbutton.addEventListener("click", changeTextColor);
-  editbutton.addEventListener("click", editToDo);
+    let editButton = document.createElement("button");
+    editButton.textContent = "Edit";
+    newDiv.appendChild(editButton);
 
-}
-
-
-function submitToDo() {
-  let submitButton = document.querySelector(".submit");
-  submitButton.addEventListener("click", submitForm);
-}
-
-function filterCompletedToDo() {
-
-  let display = document.querySelector(".display");
-  for (let i = 0; i < display.children.length; i++) {
-    if (!display.children[i].classList.contains("doneToDo")) {
-      display.children[i].style.display = "none"
-    }
+    display.appendChild(newDiv);
   }
 }
 
-function filterPendingOnlyToDo() {
-  let display = document.querySelector(".display");
-  for (let i = 0; i < display.children.length; i++) {
-    if (display.children[i].classList.contains("doneToDo")) {
-      display.children[i].style.display = "none"
-    }else{
-   
-      display.children[i].style.display = "flex"
-    
-    }
-  }
-
-}
-function resetFilter() {
-  let display = document.querySelector(".display");
-  for (let i = 0; i < display.children.length; i++) {
-    display.children[i].style.display = "flex";
-  }
-}
+let toDo = [{ title: 'sad', id: getid(), status: 'PENDING' }];
+render(toDo);
 
 
-let filterToDo = document.querySelector(".filterToDo");
-filterToDo.addEventListener("click", filterCompletedToDo);
-let filterByPending = document.querySelector(".filterByPending");
-filterByPending.addEventListener("click", filterPendingOnlyToDo);
 
-
-let allfilterss = document.querySelector(".Allfilters");
-allfilterss.addEventListener("click", resetFilter);
-
-submitToDo();
+ 
