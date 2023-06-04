b1a0e06b49b51cb3426267469d3226a8ce32ced6
DavitGalstian <davgalstiannn@gmail.com>
2023-06-05 01:55:57 +0400

    inprogress

--- index.js
+++ index.js
@@ -4,112 +4,192 @@ function generateId() {
     return id++;
   };
 }
-let getid = generateId();
-let toDo = [
-  { title: 'sad', id: getid(), status: 'pending' },
-  { title: 'gwv', id: getid(), status: 'done' },
-  { title: 'daa', id: getid(), status: 'pending' },
-  { title: 'gaa', id: getid(), status: 'done' }
-];
 
+function getTodos() {
+  const todos = localStorage.getItem('todos');
+  return JSON.parse(todos) || [];
+}
 
-function render(toDo) {
+const getID = generateId();
+let todos = getTodos();
 
+function render(todos) {
+
+  console.log(todos)
   let display = document.querySelector(".display");
   display.innerHTML = '';
-  for (let i = 0; i < toDo.length; i++) {
-    let newDiv = document.createElement("div");
-    newDiv.classList.add("toDoListDiv");
 
-    let newText = document.createElement("p");
-    newText.textContent = toDo[i].title;
-    newText.classList.add("toDoListP");
-    newDiv.appendChild(newText);
-    if (toDo[i].status == "done") {
-      newText.style.color = "green"
+  for (let i = 0; i < todos.length; i++) {
+    let newDiv = document.createElement("div");
+    newDiv.classList.add("to-do-list");
+    let newinput = document.createElement("input")
+    if (todos[i].status == "editing") {
+
+
+      newinput.value = todos[i].title;
+      newDiv.appendChild(newinput);
+    } else {
+      let newText = document.createElement("p");
+      newText.textContent = todos[i].title;
+      newText.classList.add("toDoListP");
+      newDiv.appendChild(newText);
+      if (todos[i].status == "done") {
+        newText.style.color = "green";
+      }
     }
+
+
     let deleteButton = document.createElement("button");
     deleteButton.textContent = "Delete";
     newDiv.appendChild(deleteButton);
+    deleteButton.addEventListener("click", () => deleteToDo(todos[i].id));
 
-    deleteButton.addEventListener("click", () => deleteToDo(toDo[i].id))
     let doneButton = document.createElement("button");
-
-
     doneButton.textContent = "Done";
     newDiv.appendChild(doneButton);
-    doneButton.addEventListener("click", () => markAsDone(toDo[i].id))
-    let editButton = document.createElement("button");
-    editButton.textContent = "Edit";
-    newDiv.appendChild(editButton);
+    doneButton.addEventListener("click", () => markAsDone(todos[i].id));
 
+    if (todos[i].status == "editing") {
+
+      let saveButton = document.createElement("button")
+      saveButton.textContent = "Save";
+
+      newDiv.appendChild(saveButton);
+      saveButton.addEventListener("click", function () {
+        saveToDo(todos[i].id, newinput.value);
+      });
+    } else {
+
+      let editButton = document.createElement("button");
+      editButton.textContent = "Edit";
+      newDiv.appendChild(editButton);
+
+      editButton.addEventListener("click", function () {
+        editToDo(todos[i].id)
+      })
+
+      let inProgressButton = document.createElement("button");
+      inProgressButton.textContent = "inProgress";
+      newDiv.appendChild(inProgressButton);
+      
+      inProgressButton.addEventListener("click", function () {
+        filterinprogres(todos[i].id);
+      });
+
+    }
     display.appendChild(newDiv);
   }
 }
 
-
-function doneToDo() {
-  let newToDoByDone = [];
-  for (let i = 0; i < toDo.length; i++) {
-    if (toDo[i].status == "done") {
-      newToDoByDone.push(toDo[i]);
+function filterbystatus(status) {
+  let newtodosByPending = [];
+  for (let i = 0; i < todos.length; i++) {
+    if (todos[i].status == status) {
+      newtodosByPending.push(todos[i]);
     }
   }
-  render(newToDoByDone);
-
+  render(newtodosByPending);
+  ender(newtodosByProgress);
 }
-
-function pendingToDo() {
-  let newToDoByPending = [];
-  for (let i = 0; i < toDo.length; i++) {
-    if (toDo[i].status == "pending") {
-      newToDoByPending.push(toDo[i]);
-    }
-  }
-  render(newToDoByPending);
-}
-
 function filterAllToDo() {
-  render(toDo);
+  render(todos);
 }
 
 function deleteToDo(id) {
-  toDo = toDo.filter(todo => todo.id != id);
-  render(toDo)
-  console.log(id)
+  todos = todos.filter(todo => todo.id != id);
+  render(todos);
+  localStorage.setItem('todos', JSON.stringify(todos));
+  console.log(id);
 }
 
 function markAsDone(id) {
-  for (let i = 0; i < toDo.length; i++) {
-    if (toDo[i].id == id)
-      toDo[i].status = "done"
-
+  for (let i = 0; i < todos.length; i++) {
+    if (todos[i].id == id) {
+      todos[i].status = "done";
+    }
   }
-  render(toDo)
+  localStorage.setItem('todos', JSON.stringify(todos));
+  render(todos);
 }
+
 function sumbitToDo() {
-  let input = document.querySelector(".a")
+  let input = document.querySelector(".a");
   if (input.value == "") {
     return;
   }
-  let newToDo = { title: 'sad', id: getid(), status: 'pending' }
+  let newTodo = { title: input.value, id: getID(), status: 'pending' };
+  todos.push(newTodo);
+  localStorage.setItem('todos', JSON.stringify(todos));
+  render(todos);
+}
 
-  toDo.push(newToDo)
+function editToDo(id) {
+  for (let i = 0; i < todos.length; i++) {
+    if (todos[i].id == id) {
+      todos[i].status = "editing";
+    }
+  }
+  localStorage.setItem('todos', JSON.stringify(todos));
+  render(todos);
+}
 
+function filterinprogres(id) {
+  for (let i = 0; i < todos.length; i++) {
+    if (todos[i].id == id) {
+      todos[i].status = "inprogress";
+    }
+  }
 
-  newToDo.title = input.value
-  render(toDo)
+  render(todos);
+}
+
+function sortToDo() {
+  todos.sort((a, b) => a.title.localeCompare(b.title))
+  render(todos)
 
 }
-let filtersumbit = document.querySelector(".submit");
-console.log(filtersumbit)
-filtersumbit.addEventListener("click", sumbitToDo);
+
+function saveToDo(id, title) {
+  for (let i = 0; i < todos.length; i++) {
+    if (todos[i].id == id) {
+      todos[i].status = "pending";
+      todos[i].title = title
+    }
+  }
+  localStorage.setItem('todos', JSON.stringify(todos));
+  render(todos);
+}
+
+function filterbyInProgress() {
+  let newtodosByProgress = [];
+  for (let i = 0; i < todos.length; i++) {
+    if (todos[i].status == "inprogress") {
+      newtodosByProgress.push(todos[i]);
+    }
+  }
+  render(newtodosByProgress);
+}
+let filterbyInProgresshtml = document.querySelector(".filterbyinProgress");
+filterbyInProgresshtml.addEventListener("click", filterbyInProgress)
+
+let resetFilter = document.querySelector(".Allfilters")
+resetFilter.addEventListener("click", filterAllToDo)
+
+let Sortbutton = document.querySelector(".sortButton");
+Sortbutton.addEventListener("click", sortToDo)
+
+let submitButton = document.querySelector(".submit");
+submitButton.addEventListener("click", sumbitToDo);
+
 let filterPendingToDo = document.querySelector(".filterByPending");
-filterPendingToDo.addEventListener("click", pendingToDo);
+filterPendingToDo.addEventListener("click", function () {
+  filterbystatus("pending")
+
+});
 
 let filterToDo = document.querySelector(".filterToDo");
-filterToDo.addEventListener("click", doneToDo);
+filterToDo.addEventListener("click", function () {
+  filterbystatus("done")
 
-
-
-render(toDo);
+});
+render(todos);
